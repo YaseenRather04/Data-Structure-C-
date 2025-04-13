be9fdb309e79a5e9cce595efd4331a2cebf137f5
YaseenRather04 <2024a7r002@mietjammu.in>
2025-04-13 11:27:50 +0530

    Update reverse_linked_list.c

--- Leetcode/reverse_linked_list.c
+++ Leetcode/reverse_linked_list.c
@@ -1,42 +1,16 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     struct ListNode *next;
- * };
- */
-struct ListNode* constructor(int data){
-    
-    struct ListNode* temp = (struct ListNode*)malloc(sizeof(struct ListNode));
-
-    temp->val = data;
-    temp->next = NULL;
-    return temp;
-
-}
-
-
 struct ListNode* reverseList(struct ListNode* head) {
     if(head == NULL || head->next == NULL) return head;
 
-    struct ListNode* newListhead = NULL;
-    struct ListNode* temp = head;
+    struct ListNode* curr = head;
+    struct ListNode* next = NULL;
+    struct ListNode* prev = NULL;
 
-    while(temp != NULL){
-
-        struct ListNode* newNode = constructor(temp->val);
-
-        if(newListhead == NULL){
-            newListhead = newNode;
-        }
-
-        else{
-            newNode->next = newListhead;
-            newListhead = newNode;
-        }
-        temp = temp->next;
+    while(curr != NULL){
+        next = curr->next;
+        curr->next = prev;
+        prev = curr;
+        curr = next;
     }
 
-    return newListhead;
-    
+    return prev;     
 }
